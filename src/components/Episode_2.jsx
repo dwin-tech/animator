12d489c157660696644d7f19e66f30d6168bf842
Tigran Mehrabyan <tigran.mehrabyan.dwin@gmail.com>
2022-12-23 13:56:10 +0400

    completed episode 2

--- src/components/Episode_2.jsx
+++ src/components/Episode_2.jsx
@@ -3,6 +3,11 @@ import { Episode_2_Frame_1 } from "./episode_2/episode_2_frame_1/Episode_2_frame
 import { Episode_2_Frame_2 } from "./episode_2/episode_2_frame_2/Episode_2_frame_2";
 import { Episode_2_Frame_3 } from "./episode_2/episode_2_frame_3/Episode_2_frame_3";
 import { Episode_2_Frame_4 } from "./episode_2/episode_2_frame_4/Episode_2_frame_4";
+import { Episode_2_Frame_5 } from "./episode_2/episode_2_frame_5/Episode_2_frame_5";
+import { Episode_2_Frame_6 } from "./episode_2/episode_2_frame_6/Episode_2_frame_6";
+import { Episode_2_Frame_7 } from "./episode_2/episode_2_frame_7/Episode_2_frame_7";
+import { Episode_2_Frame_8 } from "./episode_2/episode_2_frame_8/Episode_2_frame_8";
+import { Episode_2_Frame_9 } from "./episode_2/episode_2_frame_9/Episode_2_frame_9";
 
 import { Episode_2_Header } from "./episode_2/header/Episode_2_Header";
 
@@ -26,6 +31,11 @@ export function Episode_2() {
                 <Episode_2_Frame_2/>
                 <Episode_2_Frame_3/>
                 <Episode_2_Frame_4/>
+                <Episode_2_Frame_5/>
+                <Episode_2_Frame_6/>
+                <Episode_2_Frame_7/>
+                <Episode_2_Frame_8/>
+                <Episode_2_Frame_9/>
                 <Footer />
             </article>
         </div>
---
+++ src/components/episode_2/episode_2_frame_5/index.js
@@ -0,0 +1,62 @@
+export const welcomeNerg = {
+  hidden: {
+    top: "-90%",
+    left: "100%",
+    width: "80%",
+    opacity: 1,
+  },
+  show: {
+    width: "70%",
+    top: "0%",
+    left: "0%",
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.9,
+      forwards: 1,
+      ease: "linear",
+    },
+  },
+};
+
+export const welcomeCurl = {
+  hidden: {
+    top: "-85%",
+    left: "100%",
+    width: "100%",
+    opacity: 1,
+  },
+  show: {
+    width: "100%",
+    top: "0%",
+    left: "-0.5%",
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 0.9,
+      forwards: 1,
+      ease: "linear",
+    },
+  },
+};
+
+export const welcomeBlonde = {
+  hidden: {
+    top: "-60%",
+    left: "99.9%",
+    width: "80%",
+    // opacity: 1,
+  },
+  show: {
+    width: "66%",
+    top: "28%",
+    left: "34%",
+    // opacity: 1,
+    transition: {
+      delay: 2.5,
+      duration: 0.9,
+      forwards: 1,
+      ease: "linear",
+    },
+  },
+};
