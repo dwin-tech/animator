f3f20e743245e88ca432dc85d64b635d53a9b6e5
Seryozha Khachatryan <seryozha95@github.com>
2022-12-30 11:22:55 +0400

    Scroll test

--- src/components/Episode_1.jsx
+++ src/components/Episode_1.jsx
@@ -31,7 +31,7 @@ const Item = styled.div`
   align-items: center;
   color: #fff;
   width: 100%;
- background: black;
+ background: red;
  height:100vh;
 `;
 
@@ -53,44 +53,98 @@ export function Episode_1() {
   const carousel = useRef(null)
 
   return (
-    <div onWheel={debouncedChangeHandler}>
-      <Carousel
-        ref={carousel}
-        style={{ magin: 0 }}
-        // easing="cubic-bezier(1,.15,.55,1.54)"
-        // tiltEasing="cubic-bezier(0.110, 1, 1.000, 0.210)"
-        transitionMs={1400}
-        // itemPadding={[20, 0, 0, 0]}
-        verticalMode
-        pagination={false}
-        itemsToShow={1}
-        showArrows={false}
-        onChange={(_, pageIndex) => { setActiveIndex(pageIndex) }}
-        itemPosition={consts.CENTER}
-        preventDefaultTouchmoveEvent={true}
-      >
-        <Item><Episode_1_Header /></Item>
-        <Item><Frame_1 visible={activeIndex === 1} /></Item>
-        <Item><Frame_2 visible={activeIndex === 2} /></Item>
-        <Item><Frame_3 visible={activeIndex === 3} /></Item>
-        <Item><Frame_4 visible={activeIndex === 4} /></Item>
-        <Item><Frame_5 visible={activeIndex === 5} /></Item>
-        <Item><Frame_6 visible={activeIndex === 6} /></Item>
-        <Item><Frame_7 visible={activeIndex === 7} /></Item>
-        <Item><Frame_8 visible={activeIndex === 8} /></Item>
-        <Item><Frame_9 visible={activeIndex === 9} /></Item>
-        <Item><Frame_10 visible={activeIndex === 10} /></Item>
-        <Item><Frame_11 visible={activeIndex === 11} /></Item>
-        <Item><Frame_12 visible={activeIndex === 12} /></Item>
-        <Item><Frame_13 visible={activeIndex === 13} /></Item>
-        <Item><Frame_14 visible={activeIndex === 14} /></Item>
-        <Item><Frame_15 visible={activeIndex === 15} /></Item>
-        <Item><Frame_16 visible={activeIndex === 16} /></Item>
-        <Item><Frame_17 visible={activeIndex === 17} /></Item>
-        <Item><Frame_18 visible={activeIndex === 18} /></Item>
-        <Item><Footer /></Item>
-      </Carousel>
-    </div>
+    //   <div onWheel={debouncedChangeHandler}>
+    //     <Carousel
+    //       ref={carousel}
+    //       style={{ magin: 0 }}
+    //       // easing="cubic-bezier(1,.15,.55,1.54)"
+    //       // tiltEasing="cubic-bezier(0.110, 1, 1.000, 0.210)"
+    //       transitionMs={1400}
+    //       // itemPadding={[20, 0, 0, 0]}
+    //       verticalMode
+    //       pagination={false}
+    //       itemsToShow={1}
+    //       showArrows={false}
+    //       onChange={(_, pageIndex) => { setActiveIndex(pageIndex) }}
+    //       itemPosition={consts.CENTER}
+    //       preventDefaultTouchmoveEvent={true}
+    //     >
+    //       <Item><Episode_1_Header /></Item>
+    //       <Item><Frame_1 visible={activeIndex === 1} /></Item>
+    //       <Item><Frame_2 visible={activeIndex === 2} /></Item>
+    //       <Item><Frame_3 visible={activeIndex === 3} /></Item>
+    //       <Item><Frame_4 visible={activeIndex === 4} /></Item>
+    //       <Item><Frame_5 visible={activeIndex === 5} /></Item>
+    //       <Item><Frame_6 visible={activeIndex === 6} /></Item>
+    //       <Item><Frame_7 visible={activeIndex === 7} /></Item>
+    //       <Item><Frame_8 visible={activeIndex === 8} /></Item>
+    //       <Item><Frame_9 visible={activeIndex === 9} /></Item>
+    //       <Item><Frame_10 visible={activeIndex === 10} /></Item>
+    //       <Item><Frame_11 visible={activeIndex === 11} /></Item>
+    //       <Item><Frame_12 visible={activeIndex === 12} /></Item>
+    //       <Item><Frame_13 visible={activeIndex === 13} /></Item>
+    //       <Item><Frame_14 visible={activeIndex === 14} /></Item>
+    //       <Item><Frame_15 visible={activeIndex === 15} /></Item>
+    //       <Item><Frame_16 visible={activeIndex === 16} /></Item>
+    //       <Item><Frame_17 visible={activeIndex === 17} /></Item>
+    //       <Item><Frame_18 visible={activeIndex === 18} /></Item>
+    //       <Item><Footer /></Item>
+    //     </Carousel>
+    //   </div>
+    <section className="container">
+      <div className="tiktok">
+        {/* <section> */}
+        <Episode_1_Header />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+
+
+
+      <div className="tiktok">
+        {/* <section> */}
+        <Frame_1 visible={true} />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+
+      <div className="tiktok">
+        {/* <section> */}
+        <Frame_5 visible={true} />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+
+
+      <div className="tiktok">
+        {/* <section> */}
+        <Frame_2 visible={true} />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+
+      <div className="tiktok">
+        {/* <section> */}
+        <Frame_3 visible={true} />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+      <div className="tiktok">
+        {/* <section> */}
+        <Frame_6 visible={true} />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+      <div className="tiktok">
+        {/* <section> */}
+        <Footer />
+        {/* </section> */}
+        {/* <script async src="https://www.tiktok.com/embed.js"></script> */}
+      </div>
+    </section>
+    
+
+
 
   );
 }
